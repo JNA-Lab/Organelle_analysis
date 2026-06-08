796b438223f9b305891d13ad5eebf7debb190d18
fl299 <fl299@cimr.cam.ac.uk>
2026-06-08 17:06:10 +0100

    Update to handle file names with or without extensions

--- Organelle_analysis_fluorescence_quantification.py
+++ Organelle_analysis_fluorescence_quantification.py
@@ -20,13 +20,14 @@ print("Identifying files...")
 all_ROI_files = [ f for f in os.listdir(ROIdir) if os.path.isfile(ROIdir + "/" + f) and re.match('.+_ALL_ORGANELLE_ROIs\.zip$', f) and not re.match('Thumbs.db', f) ]#includes cell ROIs
 img_files = [ f for f in os.listdir(imgdir) if os.path.isfile(imgdir + "/" + f) and not re.match('^\.', f) and not re.match('Thumbs.db', f) ]
 
+
 #create analysis folder for output
 if not os.path.exists(ROIdir + "/fluorescence_quantification/"):
 	os.mkdir(ROIdir + "/fluorescence_quantification/")
 
 
 #get number of channels from first image - might break if different images have different numbers of channels
-print("Processing channels...")
+print("Checking channels...")
 img1 = IJ.openImage(os.path.join(imgdir, img_files[0]))
 channels = img1.getDimensions()[2]#width, height, nChannels, nSlices, nFrames
 #calling nChannels field directly doesn't work - BioFormats issue; see https://forum.image.sc/t/loci-plugin-in-colorizer-when-importing-through-bioformats/27183/2
@@ -50,11 +51,13 @@ if options.wasOKed():
 
 #matching image files to ROI files
 ROI_dict = dict()
+img_dict = dict()
 for f in img_files:
-	ROI_files = [r for r in all_ROI_files if f in r]
+	f_noext = re.sub('\.[A-Za-z0-9]+$', '', f)
+	ROI_files = [r for r in all_ROI_files if f_noext in r]
 	if len(ROI_files) > 0:
-		ROI_dict[f] = ROI_files[0]#should only be one
-
+		ROI_dict[f_noext] = ROI_files[0]#should only be one
+		img_dict[f_noext] = f
 
 #set up ROI manager, don't display
 RM = RoiManager(False)
@@ -62,23 +65,22 @@ rm = RM.getRoiManager()
 IJ.run("Set Measurements...", "mean min integrated display redirect=None decimal=3")#TODO - restore afterwards?
 
 
-for f in ROI_dict.keys():#will skip any image files without matching ROIs - e.g. excluded from analysis
-	img = IJ.openImage(imgdir + '/' + f)#os.path.join not working - string vs list input?
+for f in img_dict.keys():#will skip any image files without matching ROIs - e.g. excluded from analysis
+	print("Processing image " + img_dict[f])
+	img = IJ.openImage(imgdir + '/' + img_dict[f])#os.path.join not working - string vs list input?
 	rm.open(ROIdir + '/' + ROI_dict[f])
 	for c,n in to_quantify.items():
 		img.setC(c)
 		rm.runCommand("Select All")
 		rm.runCommand(img, "Measure")#TODO - calculate without bringing up results table
 		results = ResultsTable.getResultsTable()
-		results.save(ROIdir + "/fluorescence_quantification/" + f + "_" + n + "_quantification.csv")
+		results.save(ROIdir + "/fluorescence_quantification/" + img_dict[f] + "_" + n + "_quantification.csv")
 	img.close()
 	rm.reset()
 	results.reset()
 
 rm.close()
-IJ.selectWindow("Results")
-#can't select when not displayed
-IJ.run("Close")
+print("Finished")
 
 
 
